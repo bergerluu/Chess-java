3b06e5541defcb72873dd0958e881e2a3c432f54
bergerluu <luizfelipeberger15@gmail.com>
2024-06-03 19:13:31 -0300

    add king moves

--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -25,6 +25,15 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);  
+        return board.piece(position).possibleMoves();
+    }
+
+
+
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targePosition){
         Position source = sourcePosition.toPosition();
         Position target = targePosition.toPosition();
