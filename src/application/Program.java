7fffd895bfa026bf764c3d72e298eafb2622c57a
bergerluu <luizfelipeberger15@gmail.com>
2024-06-03 19:50:12 -0300

    capturing pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -11,9 +13,10 @@ import chess.ChessPosition;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
 
 
 
@@ -21,7 +24,7 @@ public class Program {
         while (true) {
             try{
             UI.clearScreen();
-            UI.printMatch(chessMatch);
+            UI.printMatch(chessMatch, captured);
             System.out.println();
             System.out.print("Source: ");
             ChessPosition source = UI.readChessPosition(sc);
@@ -35,6 +38,10 @@ public class Program {
             ChessPosition target = UI.readChessPosition(sc);
 
             ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+            if (capturedPiece != null) {
+                captured.add(capturedPiece);
+            }
             } catch (ChessException e){
                  System.out.println(e.getMessage());
                  sc.nextLine();
